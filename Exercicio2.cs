c362f4341f3c617e6350aa3e1cec86d2fdaf2bc7
root <brbxd@yahoo.com.br>
2017-07-27 18:03:02 -0300

    novo arquivo

---
+++ Exercicio2.cs
@@ -0,0 +1,12 @@
+//Exercício 2:
+public class Exercicio1
+{
+    public static void Main()
+    {
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        int d = (a + b + c) / 3;
+        System.Console.WriteLine("A média dos 3 números é: "+ d);
+    }
+}
